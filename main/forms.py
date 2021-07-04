1ba8ad62e09892ea8e0dbcd8426cafc8ed88d9ac
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2021-07-04 02:05:30 -0400

    Basic login

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,11 @@
+import re
 from django import forms
 from .models import User
+import bcrypt
+
+email_regex = r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$'
+username_regex = r'[A-Za-z0-9_]+'
+
 
 class RegisterForm(forms.ModelForm):
 
@@ -20,9 +26,48 @@ class RegisterForm(forms.ModelForm):
 
     def clean_email(self):
         email = self.cleaned_data['email']
+        print(email)
+        if not re.match(email_regex, email):
+            raise forms.ValidationError(
+                'Correo no válido'
+            )
         user = User.objects.filter(email=email)
         if user:
             raise forms.ValidationError(
                 'El correo ya ha sido ocupado por otro usuario'
             )
         return email
+
+
+class LoginForm(forms.Form):
+
+    email_or_username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email_or_username = cleaned_data.get('email_or_username')
+        password = cleaned_data.get('password')
+
+        user = None
+
+        if '@' in email_or_username:
+            if re.match(email_regex, email_or_username):
+                user = User.objects.filter(email=email_or_username)
+            else:
+                raise forms.ValidationError('Correo no válido')
+
+        else:
+            if re.match(username_regex, email_or_username):
+                user = User.objects.filter(username=email_or_username)
+            else:
+                raise forms.ValidationError('Nombre de usuario no válido')
+
+        if not user:
+            raise forms.ValidationError('El usuario no existe')
+        user = user[0]
+
+        if not bcrypt.checkpw(password.encode(), user.password_hash.encode()):
+            raise forms.ValidationError('Contraseña incorrecta')
+
+        return cleaned_data
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 from .models import User
 import bcrypt
 
@@ -17,6 +17,7 @@ def register(request):
             ).decode()
             new_user.password_hash = password_hash
             new_user.save()
+            request.session['user_id'] = new_user.id
             return redirect('home')
 
     return render(request, template_name='register.html',
@@ -24,8 +25,31 @@ def register(request):
 
 
 def login(request):
-    pass
+
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email_or_username = request.POST['email_or_username']
+            if '@' in email_or_username:
+                user = User.objects.get(email=email_or_username)
+            else:
+                user = User.objects.get(username=email_or_username)
+            request.session['user_id'] = user.id
+            return redirect('home')
+
+    return render(request, template_name='login.html',
+                  context={'form': form})
+
+
+def logout(request):
+    del request.session['user_id']
+    return redirect('login')
 
 
 def home(request):
-    return HttpResponse("ok")
+    if 'user_id' in request.session:
+        return HttpResponse("user_id = " + str(request.session['user_id']))
+    else:
+        return HttpResponse("sesión no iniciada")
