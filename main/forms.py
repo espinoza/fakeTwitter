4e0a52126a014638a48fd2c1366c4110e5eb8488
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2021-07-06 20:53:59 -0400

    Improve login style and validation

--- main/forms.py
+++ main/forms.py
@@ -114,20 +114,28 @@ class LoginForm(forms.Form):
             if re.fullmatch(email_regex, email_or_username):
                 user = User.objects.filter(email=email_or_username)
             else:
-                raise forms.ValidationError('Correo no válido')
+                raise forms.ValidationError(
+                    {'email_or_username': 'Correo no válido'}
+                )
 
         else:
             if re.fullmatch(username_regex, email_or_username):
                 user = User.objects.filter(username=email_or_username)
             else:
-                raise forms.ValidationError('Nombre de usuario no válido')
+                raise forms.ValidationError(
+                    {'email_or_username': 'Nombre de usuario no válido'}
+                )
 
         if not user:
-            raise forms.ValidationError('El usuario no existe')
+            raise forms.ValidationError(
+                {'email_or_username': 'Nombre de usuario no registrado'}
+            )
         user = user[0]
 
         if not bcrypt.checkpw(password.encode(), user.password_hash.encode()):
-            raise forms.ValidationError('Contraseña incorrecta')
+            raise forms.ValidationError(
+                {'password': 'Contraseña incorrecta'}
+            )
 
         return cleaned_data
 
